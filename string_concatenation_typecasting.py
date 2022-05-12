8d9d844b54d9a746a91862b0bec7c15ac52124a9
karthik0719 <104984916+karthik0719@users.noreply.github.com>
2022-05-12 17:09:28 +0530

    Add files via upload

---
+++ string_concatenation_typecasting.py
@@ -0,0 +1,32 @@
+# """
+# Name: Karthik
+# Date: 12th May 2022
+# Day: Thursday
+# Time: 5:00-5:30PM
+# File Description: String concatenation and type comversion
+# """
+a="Karthik"; b=7; c=1.0
+# print(a,b)  Comma in between varibales will result in space 
+# print(b,c)
+
+# print(''.isspace())  isspace is used to find weather there is space in the strings
+# print(' '.isspace())
+
+# print("h""i")
+# print("h"   "i")
+# print("h"+"i")
+# print("h"+""+"i")
+# print("h"+" "+"i")
+
+# print(a+"7")
+# print(a+" "+"7")
+
+# print("python"+"1")
+# print("python"+" "+"1")
+
+print(a+str(b))
+print(a+'_'+str(b))
+
+
+
+
